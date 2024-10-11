577fef8682e87e413916f6881ed2d0f3f6a08a62
Liam Keegan <liam@keegan.ch>
2024-10-11 15:56:48 +0200

    update subtitle, resolves #23

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -36,7 +36,7 @@ function openModalSignup() {
     <fwb-jumbotron
       header-text="predicTCR v2"
       header-classes="text-slate-100"
-      sub-text="Prediction of tumor-reactive T cell receptors from scRNA-seq data for personalized T cell therapy."
+      sub-text="Prediction of tumor-reactive T cells and TCRs from scRNA-seq data."
       sub-text-classes="text-slate-300"
       class="mb-4 bg-slate-800 bg-splash bg-no-repeat bg-center bg-cover md:rounded-lg"
     >
